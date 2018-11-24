187f50edcde9a816ec99321b54172f5e1bcb41ae
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2018-11-23 18:53:35 -0800

    Updated UI

--- app/src/main/java/com/nocholla/recyclerviewtwofragments/FragmentLeft.java
+++ app/src/main/java/com/nocholla/recyclerviewtwofragments/FragmentLeft.java
@@ -1,47 +1,28 @@
 package com.nocholla.recyclerviewtwofragments;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.app.Activity;
 import android.graphics.Color;
 
 public class FragmentLeft extends Fragment {
 
-    private OnColorChooseListener mOnColorChooseListener;
     private Button blackButton;
     private Button redButton;
     private Button greenButton;
     private Button blueButton;
     private Button grayButton;
 
-//    public static FragmentLeft newInstance() {
-//        FragmentLeft fragment = new FragmentLeft();
-//        return fragment;
-//    }
-//
-//    public FragmentLeft() {
-//        // Required empty public constructor
-//    }
-//
-//    @Override
-//    public void onAttach(Activity activity) {
-//        super.onAttach(activity);
-//        mOnColorChooseListener = (OnColorChooseListener) activity;
-//    }
+    public FragmentLeft() {
+        // Required empty public constructor
+    }
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View retView = inflater.inflate(R.layout.fragment_left, container);
 
@@ -205,22 +186,6 @@ public class FragmentLeft extends Fragment {
         }
 
         return retView;
-    }
-
-    @Override
-    public void onDestroy() {
-        mOnColorChooseListener = null;
-        super.onDestroy();
-    }
-
-    private void updateDisplay(int color) {
-        if (mOnColorChooseListener != null) {
-            mOnColorChooseListener.onColorChosen(color);
-        }
-    }
-
-    public interface OnColorChooseListener {
-        void onColorChosen(int color);
     }
 
 }
